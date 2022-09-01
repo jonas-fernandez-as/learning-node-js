0807ddfe2f978a1bbcbee6044ac00a1925d38b91
Jonas Fernandez <hemm_@hotmail.com>
2022-09-01 20:48:45 -0300

    mi primer commit

---
+++ saludar-app.js
@@ -0,0 +1,5 @@
+const saludo = require("./saludo.js") //obtenemos la importacion
+
+console.log(saludo);
+
+console.log(saludo.saludar("Jony Terere"))
---
+++ saludo.js
@@ -0,0 +1,9 @@
+function saludar (nombre){
+ return `Hola ${nombre}`
+}
+
+//console.log(module.exports)
+
+module.exports.saludar= saludar; //definimos la exportacion
+
+//console.log(module.exports)
